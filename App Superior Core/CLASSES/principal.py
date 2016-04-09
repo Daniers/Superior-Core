dded0927946770f5e7726e586eb86bc06e6246a7
bayron danilo ortiz foronda <danilo@linux-ryxx.suse>
2016-04-09 16:45:57 -0500

    resolved #45

--- App Superior Core/CLASSES/principal.py
+++ App Superior Core/CLASSES/principal.py
@@ -11,14 +11,25 @@ except AttributeError:
 
 
 class Principal(QtGui.QMainWindow):
+    """
+        Clase que hace referencia a la ventana principal de la aplicación y
+        todas las funciones. (Aún en desarrollo)
 
-    def __init__(self, permisos, parent=None):
-        super(Principal, self).__init__(parent)
+        Args:
+            permisos (Gmail API Service Object): Servicio de autorización
+                                                de acceso a la cuenta.
+
+        Attributes:
+            principal (Ui_principal): Ventana principal del entorno en Qt.
+            gmail_service :Servicio de autorización de acceso a la cuenta gmail
+    """
+    def __init__(self, permisos):
+        super(Principal, self).__init__()
         self.principal = Ui_principal()
         self.principal.setupUi(self)
         self.gmail_service = permisos
-        results= self.gmail_service.users().getProfile(userId='me').execute()
+        results = self.gmail_service.users().getProfile(userId='me').execute()
         user = results.get('emailAddress',[])
-        total_msg = results.get("messagesTotal",[])
+        total_msg = results.get('messagesTotal',[])
         self.principal.lb_usuario.setText(user)
         self.principal.lb_total.setText(str(total_msg))
--- App Superior Core/main.py
+++ App Superior Core/main.py
@@ -3,15 +3,18 @@
 from PyQt4 import QtGui
 import sys
 
-
 from CLASSES.login import Login
 from CLASSES.principal import Principal
 
 
 # Funcion principal del programa
 def main():
-    app= QtGui.QApplication(sys.argv)
-    log= Login()
+    """
+        Función principal main de la aplicación, ejecuta la acción de
+        lanzamiento de la aplicación y el entorno gráfico.
+    """
+    app = QtGui.QApplication(sys.argv)
+    log = Login()
 
     if log.exec_() == QtGui.QDialog.Accepted:
         principal = Principal(log.get_permisos())
